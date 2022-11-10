528801e13d88512d6840a26506dab4cd9c5817a7
Andrei Stefan <andry4you@hotmail.it>
2022-11-11 00:24:21 +0100

    chore: redability of the code

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 import random
 import string
 import os
+import time
 
 def game_condition(temp_word: string, lives: int, selected_word: string) -> bool:
     
     if selected_word == temp_word:
-        print("You won, you guessed the word!")
+        print(f"You won, you guessed the word {selected_word}")
         return False
 
     elif lives==0:
-        print("Game over! You lost all lives!")
+        print(f"Game over! You lost all lives. The word was {selected_word}")
         return False
         
     return True
@@ -18,42 +19,41 @@ def game_condition(temp_word: string, lives: int, selected_word: string) -> bool
 def main(play_game: bool) -> None:
 
     while play_game:
-        os.system('cls')
         lives = 7
         alphabet = list(string.ascii_lowercase) 
         f = open('list_of_words.txt', "r") #list of all words
         words = f.readlines()
 
-        _selected_word = words[random.randint(0,len(words)-1)]
+        random_word = words[random.randint(0,len(words)-1)]
 
-        selected_word = _selected_word.replace("\n","") #word to guess
+        selected_word = random_word.replace("\n","") #word to guess
 
-        shown_word = ""
+        shown_word = "" #the word with '?' to show progressively
 
         for i in selected_word:
             shown_word+='?'
 
         while game_condition(shown_word, lives,selected_word):
 
+            os.system('cls')
             current_alphabet = ""
             for i in alphabet:
                 current_alphabet+= i + " "
 
-            print("Here are your available letters: " + current_alphabet)
+            print(f"Available letters: {current_alphabet}")
 
-            print("\nThe word to guess is: " + shown_word + "\tRemaining lives: " + str(lives) + "")
+            print("\nThe word to guess is: " + shown_word + "\tLives: " + str(lives) + "")
 
             input_choice = input("Choose a letter: ")
-
-            print("------------------------------------------------")
 
             try: #try to remove a letter from the alphabet
                 alphabet.remove(input_choice)
             except:
                 print("Not available letter!")
+                time.sleep(1)
                 continue
 
-            #update shown word, remove some ?, if needed, and replace them with input_choice
+            #update shown word, remove some '?', if needed, and replace them with input_choice
             indexs_of_letters = []
             if input_choice in selected_word:
                 for letter in range(len(selected_word)):
@@ -64,7 +64,7 @@ def main(play_game: bool) -> None:
             else: 
                 lives-=1
         else:
-            if(input("\nPlay again? [y/n]: ") == 'n'):
+            if(input("\nPress 'q' + 'ENTER' to quit the game: ") == 'q'):
                 play_game=False
 
     f.close()
